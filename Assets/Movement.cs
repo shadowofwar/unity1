f56fec13795057406e34a9481ef0d4d9cf17ff5a
shadowofwar <imranrules10@gmail.com>
2015-04-12 14:25:11 -0500

    Added Terrain and Textures

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	
+	public float speed;
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		if( movement != Vector3.zero) {
+			GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+		}
+	}
+}
